9cda5f2668b26364704bee9eca0dbf90af64a9c2
IvanovYurii <ivanovyura76@gmail.com>
2024-11-02 00:34:19 +0200

    Solution of this task

--- src/main/kotlin/mate/academy/RemoveChars.kt
+++ src/main/kotlin/mate/academy/RemoveChars.kt
@@ -1,6 +1,9 @@
 package mate.academy
 
 fun removeChars(str: String): String {
-    // Implement this function
-    return ""
+    return if (str.length > 2) {
+        str.drop(1).dropLast(1) // Видаляємо перший і останній символ
+    } else {
+        "" // Якщо рядок занадто короткий, повертаємо порожній рядок
+    }
 }
